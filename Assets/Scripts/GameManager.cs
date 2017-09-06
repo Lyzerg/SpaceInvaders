13a35adaeb5c09712a9f11b86e2f0c7b5e46b059
Lyzerg <31682095+Lyzerg@users.noreply.github.com>
2017-09-06 12:35:59 -0500

    v1
    PlayerMovement
    EnemySpawner
    BulletInstantiate

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField]GameObject enemy;
+
+	public static GameManager GM;
+
+	void Awake()
+	{
+		if (GM == null)
+		{
+			GM = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void Start(){
+		Spawn ();
+	}
+
+	void Spawn()
+	{
+		for (int j = 0; j <5; j++){
+			for (int i = -5; i < 6; i++) {
+				Instantiate (enemy, new Vector2(i,j),Quaternion.identity);	
+			}
+		}
+			
+			
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] float speed;
+	[SerializeField] GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Vector2.left * Time.deltaTime * speed);	// falta limitar al jugador en X
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector2.right * Time.deltaTime * speed);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate (bullet,transform.position, Quaternion.identity);			//Toca Saber la Pos del Jugador
+		}
+	}
+}
